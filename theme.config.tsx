770031986fd98954c6e0032933b0fcf78737ba65
MarkCodering <mark.chen.sstm@gmail.com>
2024-03-20 10:52:43 +0800

    Update navigation and remove unnecessary pages

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span style={{ fontWeight: 'bold' }}>🍳 RAG Cookbook</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://ragcookbook.vercel.app',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/MarkCodering/rag-cookbook', 
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'RAG Cookbook - Made by Mark in Taiwan with ❤️',
   },
 }
 
